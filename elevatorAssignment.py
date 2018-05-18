27d332d0ca7e32d71c67a4f653d3a8e21020b242
kiwookna <xellerd@gmail.com>
2018-05-18 18:43:20 +0900

    roughdraft

---
+++ elevatorAssignment.py
@@ -0,0 +1,7 @@
+class Elevator:
+    def __init__(self, passenger, direction, destination):
+        self.passenger = passenger
+        self.direction = direction
+        self.destination = destination
+
+    
