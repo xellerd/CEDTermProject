81e9e67fadfc921a4ba7e497e4373a31d03f577a
xellerd <38971052+xellerd@users.noreply.github.com>
2018-05-19 13:35:32 +0900

    rough

--- elevatorAssignment.py
+++
@@ -1,7 +0,0 @@
-class Elevator:
-    def __init__(self, passenger, direction, destination):
-        self.passenger = passenger
-        self.direction = direction
-        self.destination = destination
-
-    
