3d7b8cf0a1f3aa88dec14069dda673515c1d9d36
GreenElaia <green_elaia@naver.com>
2018-05-23 21:22:55 +0900

    YJ

---
+++ elevator.py
@@ -0,0 +1,32 @@
+
+
+#랜덤 생성한 것을 엘리베이터 호출에 이용, 인수전달
+#시스템이 상황 확인 후 엘리베이터 배차 /올라가는리스트,내려가는리스트,승객수리스트 만듦
+#수송 리스트에 맞게 출력문 출력
+#반복
+
+
+class Elevator:
+    def __init__(self, elevatorFloor, elevatorDirection, passengerNum, passengerUpCall, passengerDownCall):
+        self.elevatorFloor = elevatorFloor #integer
+        self.elevatorDirection = elevatorDirection #integer ? 
+        self.passengerNum = passengerNum #list
+        self.passengerUpCall = passengerUpCall #list
+        self.passengerDownCall = passengerDownCall #list
+    #passengerNum , passengerUpCall, passengerDownCall  3개 list 생성
+
+
+def operation(): #상황 확인 후 엘리베이터 배차하는 함수
+        
+
+
+
+#실행 함수
+def run():
+	while 1: #특정시간(1분정도) 계속 실행시킬 거임
+	#난수 돌려서 초기값 랜덤 생성
+	#엘리베이터 클래스에 인수 전달
+
+#직접 실행시에 run()함수 실행
+if __name__ == "__main__":
+	run()
