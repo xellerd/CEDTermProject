8e7b266fa1790e1d0bc0a7da377a5cc9a4579067
kiwookna <xellerd@gmail.com>
2018-06-07 23:36:56 +0900

    newEvUpdata

--- Kiwook/Elevator.py
+++ Kiwook/Elevator.py
@@ -6,19 +6,25 @@ MOVINGTIME = 5
 STOPTIME = 10
 MAXLOAD = 20
 MAXFLOOR = 12
+OPEN = 1
+CLOSE = 0
 
 class Elevator:
     passenger = 0
     dir = STOP
+    door = 0
     destUp = []
     destDown = []
     destAdd = []
     dest = []
     floor = 1
 
-    def getOn(self): #무게 추가
+    # def getOn(self): #무게 추가
+    #
+    # def getOff(self): #무게 감소 및 목적지 리스트 요소 삭제
 
-    def getOff(self): #무게 감소 및 목적지 리스트 요소 삭제
+    def getOnOff(self):
+        self.passenger += dest[0][1]
 
     def addDest(self, onList, offList):
         listToAdd = [onList, offList]
@@ -29,11 +35,11 @@ class Elevator:
                 destDown.reverse()
                 dest = destDown + destUp + destAdd
             elif self.dir == UP:
-                if onList[0] - self.floor >= 0:
+                if onList[0] >= self.floor:
                     destUp = destUp + listToAdd
                     destUp.sort()
                     dest = destUp + destDown + offList
-                elif onList[0] - self.floor < 0:
+                elif onList[0] < self.floor:
                     destAdd = destAdd + listToAdd
                     destAdd.sort()
                     dest = destUp + destDown + destAdd
@@ -77,8 +83,26 @@ class Elevator:
 
 
     def move: #엘리베이터 방향 제어, 목적지 리스트를 기반으로 엘리베이터의 움직임 제어
-        #if dest = [] dir = stop
-        # if dest[0][0] > dest[0][1] dir = DOWN
+        if dir == STOP:
+            T = t
+        else:
+            if self.door == CLOSE:
+                if (t-T) == MOVINGTIME:
+                    if self.dir == UP:
+                        self.floor++
+                    else if self.dir == DOWN:
+                        self.floor--
+                    T = t
+                    if floor == dest[0][0]:
+                        self.getOnOff()
+                        del a[0]
+                        self.door = OPEN
+            else if self.door == OPEN:
+                if (t-T) == STOPTIME:
+                    T = t
+                    self.door = CLOSE
+
+
 
     def totalTime: #엘리베이터 승객들의 전체 예상 소요시간을 계산
 
@@ -92,8 +116,18 @@ def call(ev1, ev2, ev3, passenger, startingPoint, destination, to):
     tempEv2 = ev2
     tempEv3 = ev3
 
-    # weight cut
+    tempEv1.addDest(onList, offList)
+    tempEv2.addDest(onList, offList)
+    tempEv3.addDest(onList, offList)
 
+    # weight cut
+    expect1 = ev1.passenger
+    for i in ev1.dest:
+        if ev1.dest[i][0] <= startingPoint:
+            expect1 += dest[i][1]
+    #if expect1 > MAXLOAD->버림
+
+    # tempEv 목적지 추가
 
     #tempEv에 각각 넣어서 토탈 타임을 계산해보고 가장 적은 엘리베이터에 배차
     if tempEv1.totalTime() <= tempEv2.totalTime() and tempEv1.totalTime() <= tempev3.totalTime():
@@ -102,3 +136,25 @@ def call(ev1, ev2, ev3, passenger, startingPoint, destination, to):
         ev2.addDest(onList, offList)
     elif tempEv3.totalTime() <= tempEv1.totalTime() and tempEv3.totalTime() <= tempev2.totalTime():
         ev3.addDest(onList, offList)
+
+
+#main
+ev1 = Elevator()
+ev2 = Elevator()
+ev3 = Elevator()
+
+randomList = [index, time, passenger, startingPoint, destination, time2]
+
+t = 0
+while True:
+    ev1.move()
+    ev2.move()
+    ev3.move()
+
+    #search randomList
+    #if time == t:
+        # call(ev1, ev2, ev3, randomListElement):
+
+
+
+    t++
